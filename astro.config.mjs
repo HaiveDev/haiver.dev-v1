496094687643009a91b669d86eb36d5ff573e274
Haiver Rueda <devhaiverueda@gmail.com>
2024-06-17 03:05:48 -0500

    Omitiendo auth-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import auth from "auth-astro";
+/* import auth from "auth-astro"; */
 
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
-  integrations: [tailwind(), react(), auth()],
+  integrations: [tailwind(), react()],
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
